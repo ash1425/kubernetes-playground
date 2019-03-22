24a606d969f379518f6fca1d0712af9359f17494
Ashay Thorat <ashayt@thoughtworks.com>
2019-03-22 11:53:22 +0530

    Images are reffered by digest

--- server/src/main/java/com/ashay/trying/kub/Person.java
+++ server/src/main/java/com/ashay/trying/kub/Person.java
@@ -2,33 +2,21 @@ package com.ashay.trying.kub;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NonNull;
 import org.springframework.data.annotation.Id;
 
+@Data
 public class Person {
 
     @Id
     private String id;
 
+    @NonNull
     private String name;
 
     @JsonCreator
     public Person(@JsonProperty("name") String name) {
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 }
